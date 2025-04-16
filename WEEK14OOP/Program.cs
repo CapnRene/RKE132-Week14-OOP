4d51c4c238f75a2d922a3a06cf027d045a61fe9e
Rene Iliste <reneiliste@gmail.com>
2025-04-16 19:40:00 +0300

    ADD getters, setters and Cat

--- WEEK14OOP/Program.cs
+++ WEEK14OOP/Program.cs
@@ -1,18 +1,43 @@
-﻿Console.WriteLine("Name your dog:");
+﻿//Cat
+Cat newCat = new Cat("Garfield", "ginger");
+Console.WriteLine($"{newCat.Name} wants a rub on its belly.");
+while (newCat.BellyFull != 0
+)
+{
+    newCat.Sleep();
+}
+newCat.Meow();
+newCat.ShowCatData();
+
+//Dog
+Console.WriteLine("Name your dog:");
 string myDogName = Console.ReadLine();
 
 Dog myDog = new Dog(myDogName);
-myDog._levelOfHapiness = 10;
+myDog.NewHapiness(0);
 
 Dog Pipi = new Dog("Pipi");
-Pipi._levelOfHapiness = 5;
+Pipi.NewHapiness(5);
 
-Console.WriteLine($"{myDog._name} is a first dog. Level of hapiness: {myDog._levelOfHapiness}");
-Console.WriteLine($"{Pipi._name} is a second dog. Level of hapiness: {Pipi._levelOfHapiness}");
+Console.WriteLine($"{myDog.Name} is a first dog. Level of hapiness: {myDog.LevelOfHapiness}");
+Console.WriteLine($"{Pipi.Name} is a second dog. Level of hapiness: {Pipi.LevelOfHapiness}");
+
+myDog.Rename("Paha Pontu");
+
+myDog.Bark();
+Console.WriteLine($"My dogs level of hapiness: {myDog.LevelOfHapiness}");
+
+while (myDog.LevelOfHapiness != 5)
+{
+    myDog.Bark();
+    Console.WriteLine($"My dogs level of hapiness: {myDog.LevelOfHapiness}");
+}
+
+myDog.WagTail();
 
 class Dog {
-    public string _name = "Koer";
-    public int _levelOfHapiness = 0;
+    private string _name = "Koer";
+    private int _levelOfHapiness = 0;
 
 
     //constructor
@@ -22,4 +47,91 @@ class Dog {
         _name = name;
         _levelOfHapiness = 0;
     }
+
+    //getter
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    //setter
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"Koera uus nimi on {_name}");
+    }
+
+    public void NewHapiness(int newHapiness)
+    {
+        _levelOfHapiness = newHapiness;
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Auh-auh!");
+        _levelOfHapiness += 1;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Koer lehvitab saba");
+        _levelOfHapiness += 1;
+    }
+};
+
+
+
+class Cat
+{
+    private string _name;
+    private string _color;
+    private int _bellyFull;
+    public Cat(string name, string color)
+    {
+        _name = name;
+;
+        _color = color;
+;
+        _bellyFull = 10;
+        Console.WriteLine($"A {color} cat {name} has been created.");
+    }
+
+    public string Name //getter
+    {
+
+        get
+        {
+            return
+             _name;
+        }
+    }
+
+    public int BellyFull //getter
+    {
+
+        get
+        {
+            return _bellyFull;
+        }
+    }
+    public void Sleep()
+    {
+        _bellyFull--;
+        Console.WriteLine($"{_name} says: Z-z-Z-z-Z-z");
+    }
+    public void Meow()
+    {
+        Console.WriteLine($"{_name} says: Meow!");
+    }
+    public void ShowCatData() //displays info about the cat object
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Color: {_color}");
+        Console.WriteLine($"Level of hungriness: {_bellyFull}");
+    }
+
 }
